29a39ddd8dccd6546d97c27993c597fcad2a78ce
sampurna445 <128822770+sampurna445@users.noreply.github.com>
2023-09-30 14:51:09 +0100

    added props to HotelsDetailsScreen

--- screens/HotelsDetailsScreen/index.js
+++ screens/HotelsDetailsScreen/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 
-const HotelDetailsScreen = (route, navigation) => {
-  // const navigation = useNavigation();
-  // const hotel = navigation.getParam('hotel', {});
-
-  // const {hotelName, hotelAddress, hotelPhone, hotelWebsite} = route.params;
+const HotelDetailsScreen = props => {
+  const {hotelName, hotelAddress, hotelPhone, hotelWebsite} =
+    props.route.params;
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Detials Page</Text>
-      {/* <Text>Name: {hotelName}</Text>
+      <Text>Name: {hotelName}</Text>
       <Text>Address: {hotelAddress}</Text>
       <Text>Phone: {hotelPhone}</Text>
-      <Text>Website: {hotelWebsite}</Text> */}
+      <Text>Website: {hotelWebsite}</Text>
     </View>
   );
 };
